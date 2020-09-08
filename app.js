8bc315fd0cfa58a3562ae02f535036d4a81e3f5d
vishal chintham <chinthamvishal.2019@vitstudent.ac.in>
2020-09-08 17:17:43 +0530

    mysql has been setup

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ const bodyParser = require("body-parser");
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
+var con = mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"35791408",
+  database:"supermarket"
+});
 
+con.connect((err)=>{
+  if(err) console.log(err);
+  else console.log("successfully connected");
+})
+
+app.get("/login",(req,res)=>{
+  con.query("Select * from student",(err,rows)=>{
+    if(err) console.log(err);
+    else res.send(rows);
+  })
+})
 
 app.listen(3000,()=>{
   console.log("server started on port 3000");
-})
+});
