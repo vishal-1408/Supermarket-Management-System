ca1cfcb7b83f67f24679bf8fd1fa18ecc1bcd9f0
vishal chintham <chinthamvishal.2019@vitstudent.ac.in>
2020-09-10 20:53:29 +0530

    authentication completed

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const session = require("express-session");
 
 app.use(session({
   secret:"notthefinalsecretjustatrial",
-  resave:true,
-  uninitialized:true
-}))
+  resave:false,
+  uninitialized:false
+}));
 
 
 app.set("view engine","ejs");
@@ -29,26 +29,40 @@ con.connect((err)=>{
   else console.log("successfully connected");
 });
 
-app.get("/logout",(req,res)=>{
-  req.session.destroy();
-  res.redirect("/register");
-});
-app.get("/register",(req,res)=>{
-  res.render("register");
-});
-app.get("/",getting,(re,res)=>{
+
+
+///////////////////////////////////////////////get routes
+app.get("/",(req,res)=>{
    res.send('unlocked');
 });
 
+app.get("/register",(req,res)=>{
+  res.render("register");
 
-function getting(req,res,next){
-  if(req.user){
-    next();
-  }else{
-    res.redirect("/register");
-  }
-}
+});
 
+app.get("/login",(req,res)=>{
+    res.render("login");
+  });
+
+
+
+app.get("/secret",isAuthenticated,(req,res)=>{
+  res.send("secret!!wihieee");
+})
+
+
+app.get("/logout",(req,res)=>{
+  req.session.destroy((err)=>{
+    if(err) console.log(err);
+
+  });
+  res.clearCookie("connect.sid");
+  res.redirect("/login");
+});
+
+
+///////////////////////////////////////////////////post routes
 app.post("/register",(req,res)=>{
   bcrypt.hash(req.body.user.password,10,(e,hash)=>{
     con.query(`INSERT INTO LOGIN(username,password) VALUES('${req.body.user.username}','${hash}')`,req.body.user,(err,results)=>{
@@ -57,21 +71,49 @@ app.post("/register",(req,res)=>{
         console.log(results,results.affectedRows);
         req.session.user=req.body.user.username;
         req.user=req.body.user.username;
-        res.redirect("/");
+        res.redirect("/secret");
       }
-    })
+    });
   });
 });
 
-  app.get("/login",(req,res)=>{
-    con.query("Select * from student",(err,rows)=>{
-      if(err) console.log(err);
-      else res.send(rows);
-    })
-  });
+app.post("/login",(req,res)=>{
+  con.query(`Select password from login where username='${req.body.username}'`,(err,results)=>{
+    if(err) console.log(err);
+    else{
+      console.log(results);
+      if(results[0]){
+        bcrypt.compare(req.body.password,results[0].password,(err,resp)=>{
+          if(err) {
+            console.log(err);
+          }else {
+            if(resp==true){
+            req.session.user=req.body.username;
+            req.user=req.body.username;
+            res.redirect("/secret");
+          }else{
+            res.redirect("back");
+          }
+        }
+      })
+      }else{
+        res.redirect("/register");
+      }
+    }
+  })
+});
 
 
-
+/////////////////////////////////////////////////middlewares
+  function isAuthenticated(req,res,next){
+    console.log(req.session.user);
+    if(req.session.user){
+      next();
+    }else{
+      res.redirect("/login");
+    }
+  }
+////add already authenticated route to not give access to login or register for the user already registered or loggedin
 
 
 
